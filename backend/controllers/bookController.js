d301de2cb51056ba4c21d8e8ce92f2cc4f1019a6
SafaeElouatyq <safaeelouatyq@gmail.com>
2026-04-10 12:55:34 +0100

    complete book controller

--- backend/controllers/bookController.js
+++ backend/controllers/bookController.js
@@ -15,3 +15,70 @@ export const createBook = async (req,res)=>{
     }
 }
 
+
+export const getBooks = async (req, res) => {
+  try {
+    const books = await Book.find()
+      .populate("author", "nom prenom nationalite")
+      .populate("category", "name description");
+
+    res.status(200).json(books);
+  } catch (error) {
+    console.error("Erreur :", error);
+    res.status(500).json({ message: "Erreur serveur" });
+  }
+};
+
+export const updateBook = async (req, res) => {
+  const id = req.params.id;
+  const { title, author, description, price, stock, image, category } = req.body;
+
+  try {
+    const book = await Book.findByIdAndUpdate(
+      id,
+      {
+        title,
+        author,
+        description,
+        price,
+        stock,
+        image,
+        category,
+      },
+      { new: true }
+    )
+      .populate("author", "nom prenom nationalite")
+      .populate("category", "name description");
+
+    if (!book) {
+      return res.status(404).json({ message: "Livre non trouvé" });
+    }
+
+    res.status(200).json({
+      message: "Livre mis à jour avec succès",
+      book,
+    });
+  } catch (error) {
+    console.error("Erreur :", error);
+    res.status(500).json({ message: "Erreur serveur" });
+  }
+};
+
+export const deleteBook = async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const book = await Book.findByIdAndDelete(id);
+
+    if (!book) {
+      return res.status(404).json({ message: "Livre non trouvé" });
+    }
+
+    res.status(200).json({
+      message: "Livre supprimé avec succès",
+    });
+  } catch (error) {
+    console.error("Erreur :", error);
+    res.status(500).json({ message: "Erreur serveur" });
+  }
+};
